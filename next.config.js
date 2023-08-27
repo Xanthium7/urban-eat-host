5ffb9501be21bb4bccbd7c08cd635b6e45374e42
Akshhay <=>
2023-08-27 19:59:48 +0530

    updated next congif to normal

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
-const withTM = require("next-transpile-modules")([
-  "@mui/material",
-  "@mui/system",
-  "@mui/icons-material", // If @mui/icons-material is being used
-]);
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-module.exports = withTM({
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@mui/styled-engine": "@mui/styled-engine-sc",
-    };
-    return config;
-  },
-});
+module.exports = nextConfig;
